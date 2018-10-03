b80ca2978c44d44f72fe3a36eca333b1a0ea1961
Swapneel Kedari <43803546+SwapneelKedari@users.noreply.github.com>
2018-10-03 10:44:23 +0530

    Add files via upload

---
+++ src/main/java/myApp.java
@@ -0,0 +1,14 @@
+
+public class myApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+		for (i=0;i<10;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
